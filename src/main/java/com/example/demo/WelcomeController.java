e0a408812f78e68f30948760f362932c61d4b5ad
Priya <priyamoluv0326@gmail.com>
2022-02-18 12:17:51 +0530

    First commit

--- src/main/java/com/example/demo/WelcomeController.java
+++ src/main/java/com/example/demo/WelcomeController.java
@@ -1,12 +1,14 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
-	 @GetMapping("/welcome")
-	public String welcome() {
+	 @GetMapping("")
+	 @ResponseBody
+	 String welcome() {
 		return "Welcome to spring boot";
 	}
 
